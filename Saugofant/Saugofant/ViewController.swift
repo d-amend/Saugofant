f971646f42e5b4fe8a0060f12f61f93d6045ef7c
David Amend <david@kreuzlin.de>
2016-09-07 17:51:25 +0200

    Add empty Xcode project

---
+++ Saugofant/Saugofant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Saugofant
+//
+//  Created by David Amend on 07.09.16.
+//  Copyright © 2016 David Amend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
