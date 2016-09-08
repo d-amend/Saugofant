76a9afb39d19df418fe4e5951e98687534a1ddd9
David Amend <david@kreuzlin.de>
2016-09-08 15:16:42 +0200

    Add view controller classes

---
+++ Saugofant/Saugofant/BioViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BioViewController.swift
+//  Saugofant
+//
+//  Created by David Amend on 08.09.16.
+//  Copyright © 2016 David Amend. All rights reserved.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressCloseButton(sender: UIButton) {
+        self.dismissViewControllerAnimated(true) { 
+            
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Saugofant/Saugofant/StatusViewController.swift
@@ -0,0 +1,41 @@
+//
+//  StatusViewController.swift
+//  Saugofant
+//
+//  Created by David Amend on 08.09.16.
+//  Copyright © 2016 David Amend. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressCloseButton(sender: UIButton) {
+        
+        self.dismissViewControllerAnimated(true) { 
+            
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
